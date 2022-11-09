b985bdee431d09f955b6c13d2f08b5acffbc4942
Federico Cometto <fedecometto98@gmail.com>
2022-11-09 14:39:47 -0300

    navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,28 +4,41 @@ import { Link, Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-    <nav className=''>
-        <div className=''> 
+    <nav class="bg-gray-800">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="relative flex h-16 items-center justify-between">
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    <div class="hidden sm:ml-6 sm:block">
+                        <div class="flex space-x-4">
+                        
+                            <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">
+                                <Link to={'/'}>Home</Link>
+                            </a>
 
-            <ul className=''>
-                <li className=''>
-                    <Link to={'/'}>Home</Link>
-                </li>
-                <li className=''>
-                    <Link to={'/contacto'}>Contacto</Link>
-                </li>
-                <li className=''>
-                    <Link to={'/registrarse'}>Registrarse</Link>
-                </li>
-                <li className=''>
-                    <Link to={'/productos'}>Productos</Link>
-                </li>
+                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                <Link to={'/productos'}>Productos</Link>
+                            </a>
 
-                <Outlet />    
-            </ul>
+                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                <Link to={'/contacto'}>Contacto</Link>
+                            </a>
+                            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                                <Link to={'/registrarse'}>Registrarse</Link>
+                            </a>
+                            
+                            <Outlet /> 
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </nav>
-  )
+
+)
 }
 
 export default Layout;
+
+
+
